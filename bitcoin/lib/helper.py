11e8406a4ad96382f192d7ac63ef0c36cd6a3b42
Niu Yutong <jose.niu@hotmail.com>
2022-07-26 00:04:18 +0800

    add bitcoin tx module

--- bitcoin/lib/helper.py
+++ bitcoin/lib/helper.py
@@ -48,3 +48,32 @@ def int_to_little_endian(n, length):
     '''endian_to_little_endian takes an integer and returns the little-endian
     byte sequence of length'''
     return n.to_bytes(length, 'little')
+
+def read_varint(s):
+    '''read_varint reads a variable integer from a stream'''
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0xfd means the next two bytes are the number
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        # 0xfe means the next four bytes are the number
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        # 0xff means the next eight bytes are the number
+        return little_endian_to_int(s.read(8))
+    else:
+        # anything else is just the integer
+        return i
+
+def encode_varint(i):
+    '''encodes an integer as a varint'''
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
---
+++ bitcoin/lib/script.py
@@ -0,0 +1,97 @@
+from io import BytesIO
+
+from helper import (
+    encode_varint,
+    int_to_little_endian,
+    little_endian_to_int,
+    read_varint,
+)
+
+class Script:
+
+    def __init__(self, cmds=None):
+        if cmds is None:
+            self.cmds = []
+        else:
+            self.cmds = cmds
+    
+    @classmethod
+    def parse(cls, s):
+        # get the length of the entire field
+        length = read_varint(s)
+        # initialize the cmds array
+        cmds = []
+        # initialize the number of bytes we've read to 0
+        count = 0
+        # loop until we've read length bytes
+        while count < length:
+            # get the current byte
+            current = s.read(1)
+            # increment the bytes we've read
+            count += 1
+            # convert the current byte to an integer
+            current_byte = current[0]
+            # if the current byte is between 1 and 75 inclusive
+            if current_byte >= 1 and current_byte <= 75:
+                # we have an cmd set n to be the current byte
+                n = current_byte
+                # add the next n bytes as an cmd
+                cmds.append(s.read(n))
+                # increase the count by n
+                count += n
+            elif current_byte == 76:
+                # op_pushdata1
+                data_length = little_endian_to_int(s.read(1))
+                cmds.append(s.read(data_length))
+                count += data_length + 1
+            elif current_byte == 77:
+                # op_pushdata2
+                data_length = little_endian_to_int(s.read(2))
+                cmds.append(s.read(data_length))
+                count += data_length + 2
+            else:
+                # we have an opcode. set the current byte to op_code
+                op_code = current_byte
+                # add the op_code to the list of cmds
+                cmds.append(op_code)
+        if count != length:
+            raise SyntaxError('parsing script failed')
+        return cls(cmds)
+
+    def raw_serialize(self):
+        # initialize what we'll send back
+        result = b''
+        # go through each cmd
+        for cmd in self.cmds:
+            # if the cmd is an integer, it's an opcode
+            if type(cmd) == int:
+                # turn the cmd into a single byte integer using int_to_little_endian
+                result += int_to_little_endian(cmd, 1)
+            else:
+                # otherwise, this is an element
+                # get the length in bytes
+                length = len(cmd)
+                # for large lengths, we have to use a pushdata opcode
+                if length < 75:
+                    # turn the length into a single byte integer
+                    result += int_to_little_endian(length, 1)
+                elif length > 75 and length < 0x100:
+                    # 76 is pushdata1
+                    result += int_to_little_endian(76, 1)
+                    result += int_to_little_endian(length, 1)
+                elif length >= 0x100 and length <= 520:
+                    # 77 is pushdata2
+                    result += int_to_little_endian(77, 1)
+                    result += int_to_little_endian(length, 2)
+                else:
+                    raise ValueError('too long an cmd')
+                result += cmd
+        return result
+
+    def serialize(self):
+        # get the raw serialization (no prepended length)
+        result = self.raw_serialize()
+        # get the length of the whole thing
+        total = len(result)
+        # encode_varint the total length of the result and prepend
+        return encode_varint(total) + result
